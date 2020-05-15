4cb1402c01267794cf42cfdf16ff30e8ac5aca06
langton <langtonfavor@gmail.com>
2020-05-15 03:06:05 -0400

    created new repo

---
+++ index.js
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>markets</title>
+        <link rel="stylesheet" type="text/css" href="css/styles.css"></link>
+    </head>
+
+    <body>
+        <header>
+            <div claa="main">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Learn More</a></li>
+                </ul>
+            </div>
+        </header>
+    </body>
+</html>
